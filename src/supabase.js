687e857123a6bf7856aaf6144eacc81ab6d50074
Baraa Basim <outofrai@gmail.com>
2026-05-06 20:13:05 +0300

    Update Supabase configuration to new project

--- src/supabase.js
+++ src/supabase.js
@@ -1,7 +1,7 @@
 
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://kkoliboxrlakczkrhtax.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imtrb2xpYm94cmxha2N6a3JodGF4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjgyODc1MzEsImV4cCI6MjA4Mzg2MzUzMX0.0RUns0D1OuBHAbE-8rFLGexxQS5fr0r79Czy7sdNdB8';
+const supabaseUrl = 'https://npyzgvfasmlqwjggqqzc.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5weXpndmZhc21scXdqZ2dxcXpjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgwNzk5MjQsImV4cCI6MjA5MzY1NTkyNH0.xgnxHpXNI4k90yeYWeibsi2BCphjOUp4hgam6h0Fal4';
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
